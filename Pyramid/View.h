2ab8a2fe8003d610190bcc6705141efa7d997aff
Kathleen Urvalek <Urvalek@me.com>
2011-06-30 16:03:28 -0400

    first commit

---
+++ Pyramid/View.h
@@ -0,0 +1,16 @@
+//
+//  View.h
+//  Pyramid
+//
+//  Created by Kathleen Urvalek on 6/30/11.
+//  Copyright 2011 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface View : UIView {
+    
+}
+
+@end
